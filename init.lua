91fc0b81a0694a57c896c3de222babb71af116fe
Scott Young <syoung@staff.iinet.net.au>
2014-07-17 13:51:51 +0800

    Initial commit.

---
+++ init.lua
@@ -0,0 +1,54 @@
+require "switch"
+package.loaded["switch"] = nil   
+
+hydra.alert("config loaded", 1)
+
+pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+
+hotkey.bind({"cmd", "ctrl", "shift"}, "R", repl.open)
+hotkey.bind({"cmd", "ctrl"}, "R", hydra.reload)
+
+-- show a helpful menu
+menu.show(function()
+    local updatetitles = {[true] = "Install Update", [false] = "Check for Update..."}
+    local updatefns = {[true] = updates.install, [false] = checkforupdates}
+    local hasupdate = (updates.newversion ~= nil)
+
+    return {
+      {title = "Reload Config", fn = hydra.reload},
+      {title = "-"},
+      {title = "About", fn = hydra.showabout},
+      {title = updatetitles[hasupdate], fn = updatefns[hasupdate]},
+      {title = "Quit Hydra", fn = os.exit},
+    }
+end)
+
+-- save the time when updates are checked
+function checkforupdates()
+  updates.check()
+  settings.set('lastcheckedupdates', os.time())
+end
+
+-- show available updates
+local function showupdate()
+  os.execute('open https://github.com/sdegutis/Hydra/releases')
+end
+
+-- what to do when an update is checked
+function updates.available(available)
+  if available then
+    notify.show("Hydra update available", "", "Click here to see the changelog and maybe even install it", "showupdate")
+  else
+    hydra.alert("No update available.")
+  end
+end
+
+-- check for updates every week
+timer.new(timer.weeks(1), checkforupdates):start()
+notify.register("showupdate", showupdate)
+
+-- if this is your first time running Hydra, you're launching it more than a week later, check now
+local lastcheckedupdates = settings.get('lastcheckedupdates')
+if lastcheckedupdates == nil or lastcheckedupdates <= os.time() - timer.days(7) then
+  checkforupdates()
+end
---
+++ switch.lua
@@ -0,0 +1,71 @@
+-- Special bindings.
+local special_bindings = {
+    X = "Excel",
+    T = "iTerm",
+    W = "VMware Fusion",
+    V = "Cisco AnyConnect Secure Mobility Client"
+}
+
+local function first_letter(string)
+    return string.upper(string.sub(string, 1, 1))
+end
+
+-- Find applications starting with this letter.
+local function filter_apps(key)
+    return fnutils.filter(application.runningapplications(), function(a)
+        local it_matches = key == first_letter(a:title()) or
+                           a:title() == special_bindings[key]
+        return  it_matches and
+                a:kind() == 1 and   -- must be in the Dock
+                #a:allwindows() > 0 -- must have live windows
+    end)
+end
+
+-- If multiple apps match the key, don't repeatedly select the
+-- app that's already open.
+local function cycle_apps(apps)
+    local current_app = window.focusedwindow():application()
+    local current_app_index = nil
+    for i, a in pairs(apps) do
+        if a:title() == current_app:title() then
+            current_app_index = i
+        end
+    end
+    local index = nil
+    if not current_app_index then
+        index = 1
+    else
+        index = current_app_index + 1
+        if index > #apps then index = 1 end
+    end
+    apps[index]:activate()
+end
+
+-- Guess which of the filtered apps are wanted, and focus it.
+local function select_app(initial)
+    local apps = filter_apps(initial)
+    if #apps == 1 then
+        hydra.alert(apps[1]:title(), 1)
+        local success = apps[1]:activate()
+        if not success then hydra.alert("no windows?") end
+    elseif #apps > 1 then
+        cycle_apps(apps)
+    else
+        hydra.alert("No apps.", 1)
+    end
+end
+
+-- Set modal key.
+local modal = modalkey.new({"ctrl"}, "Y")
+modal:bind({}, "ESCAPE", function() modal:exit() end)
+
+-- Bind filter_apps to every alphanumeric key.
+local alphanum = {}
+for i=1,26  do alphanum[i] = string.char(i+64) end -- [a-z]
+for i=27,36 do alphanum[i] = string.char(i+21) end -- [0-9]
+for i=1,36 do
+    modal:bind({}, alphanum[i], function()
+        select_app(alphanum[i])
+        modal:exit()
+    end)
+end
