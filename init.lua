e4b85136d3bdcbc1632abff381e1060b03d276af
Scott Young <syoung@staff.iinet.net.au>
2014-07-22 19:33:47 +0800

    Update init.lua for new Hydra API.

--- init.lua
+++ init.lua
@@ -1,48 +1,30 @@
 hydra.alert("Hydra config loaded.", 1)
 
+-- Reload config on hotkey or config write.
 pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+hotkey.bind({"cmd", "ctrl"}, "R", hydra.reload)
 
-hotkey.bind({"cmd", "ctrl", "shift"}, "R", repl.open)
+-- Configure menu.
+hydra.menu.show(function() return {
+    {title = "Reload", fn = hydra.reload},
+    {title = "Quit", fn = os.exit},
+} end)
 
-menu.show(function()
-    return {
-      {title = "Reload", fn = hydra.reload},
-      {title = "-"},
-      {title = "About", fn = hydra.showabout},
-      {title = "Quit", fn = os.exit},
-    }
-end)
-
--- Set modal keys.
-local function set_modal_key(char, message)
-    key = modalkey.new({"ctrl"}, char)
+-- Default configuration for all modal keys.
+local function create_modal_key(char, message)
+    key = hotkey.modal.new({"ctrl"}, char)
+    key.entered = hydra.menu.highlight
+    key.exited = hydra.menu.unhighlight
+    hotkey.bind({"cmd", "ctrl"}, "W", hydra.menu.unhighlight)
     key:bind({}, "ESCAPE", function() key:exit() end)
     key:bind({"ctrl"}, char, function() key:exit() end)
     return key
 end
-ctrl_e = set_modal_key("E")
-ctrl_r = set_modal_key("R")
+ctrl_e = create_modal_key("E")
+ctrl_r = create_modal_key("R")
 
 require "switch"
 package.loaded["switch"] = nil   
 
--- Resize
 require "grid"
 package.loaded["grid"] = nil
-
-local function wrap(fn, str)
-    return function()
-        hydra.alert(str, 1); fn()
-    end
-end
-
-ctrl_r:bind({}, "[", ext.grid.pushwindow_prevscreen)
-ctrl_r:bind({}, "]", ext.grid.pushwindow_nextscreen)
-ctrl_r:bind({}, "H", wrap(ext.grid.pushwindow_left, "←"))
-ctrl_r:bind({}, "J", wrap(ext.grid.pushwindow_down, "↓"))
-ctrl_r:bind({}, "K", wrap(ext.grid.pushwindow_up, "↑"))
-ctrl_r:bind({}, "L", wrap(ext.grid.pushwindow_right, "→"))
-ctrl_r:bind({"shift"}, "L", ext.grid.resizewindow_wider)
-ctrl_r:bind({"shift"}, "H", ext.grid.resizewindow_thinner)
-ctrl_r:bind({"shift"}, "K", ext.grid.resizewindow_shorter)
-ctrl_r:bind({"shift"}, "J", ext.grid.resizewindow_taller)
