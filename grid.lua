caa2423c73c38b70f9e1881affa2720a412b63cf
Scott Young <syoung@staff.iinet.net.au>
2014-08-01 15:10:14 +0800

    Refactor grid.lua.

--- grid.lua
+++ grid.lua
@@ -5,96 +5,98 @@ ext.grid.MARGINY = 0
 ext.grid.GRIDWIDTH = 6
 ext.grid.GRIDHEIGHT = 4
 
+-- [ UTILITY ] --
+
 local function round(num, idp)
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
 
-function ext.grid.get(win)
-  local winframe = win:frame()
-  local screenrect = win:screen():frame_without_dock_or_menu()
-  local widthdelta = screenrect.w / ext.grid.GRIDWIDTH
-  local heightdelta = screenrect.h / ext.grid.GRIDHEIGHT
-  return {
-    x = round((winframe.x - screenrect.x) / widthdelta),
-    y = round((winframe.y - screenrect.y) / heightdelta),
-    w = math.max(1, round(winframe.w / widthdelta)),
-    h = math.max(1, round(winframe.h / heightdelta)),
-  }
+local function get_grid(win)
+    local win = win or window.focusedwindow()
+    if not win then hydra.alert("noop") end
+    local winframe = win:frame()
+    local screenrect = win:screen():frame_without_dock_or_menu()
+    local widthdelta = screenrect.w / ext.grid.GRIDWIDTH
+    local heightdelta = screenrect.h / ext.grid.GRIDHEIGHT
+    return {
+        x = round((winframe.x - screenrect.x) / widthdelta),
+        y = round((winframe.y - screenrect.y) / heightdelta),
+        w = math.max(1, round(winframe.w / widthdelta)),
+        h = math.max(1, round(winframe.h / heightdelta)),
+    }
 end
 
-function ext.grid.set(win, grid, screen)
-  local screenrect = screen:frame_without_dock_or_menu()
-  local widthdelta = screenrect.w / ext.grid.GRIDWIDTH
-  local heightdelta = screenrect.h / ext.grid.GRIDHEIGHT
-  local newframe = {
-    x = (grid.x * widthdelta) + screenrect.x,
-    y = (grid.y * heightdelta) + screenrect.y,
-    w = grid.w * widthdelta,
-    h = grid.h * heightdelta,
-  }
+local function set_grid_and_screen(grid, screen, win)
+    -- default args
+    win = win or window.focusedwindow()
+    screen = screen or win:screen()
 
-  newframe.x = newframe.x + ext.grid.MARGINX
-  newframe.y = newframe.y + ext.grid.MARGINY
-  newframe.w = newframe.w - (ext.grid.MARGINX * 2)
-  newframe.h = newframe.h - (ext.grid.MARGINY * 2)
-
-  win:setframe(newframe)
+    local screenrect = screen:frame_without_dock_or_menu()
+    local widthdelta = screenrect.w / ext.grid.GRIDWIDTH
+    local heightdelta = screenrect.h / ext.grid.GRIDHEIGHT
+    local newframe = {
+        x = (grid.x * widthdelta) + screenrect.x,
+        y = (grid.y * heightdelta) + screenrect.y,
+        w = grid.w * widthdelta,
+        h = grid.h * heightdelta,
+    }
+    newframe.x = newframe.x + ext.grid.MARGINX
+    newframe.y = newframe.y + ext.grid.MARGINY
+    newframe.w = newframe.w - (ext.grid.MARGINX * 2)
+    newframe.h = newframe.h - (ext.grid.MARGINY * 2)
+    win:setframe(newframe)
 end
 
-function ext.grid.snap(win)
-  if win:isstandard() then
-    ext.grid.set(win, ext.grid.get(win), win:screen())
-  end
+local function snap(win)
+    if win:isstandard() then
+        set_grid_and_screen(get_grid(win), win:screen(), win)
+    end
 end
 
-function ext.grid.adjustwidth(by)
-  ext.grid.GRIDWIDTH = math.max(1, ext.grid.GRIDWIDTH + by)
-  hydra.alert("grid is now " .. tostring(ext.grid.GRIDWIDTH) .. " tiles wide", 1)
-  fnutils.map(window.visiblewindows(), ext.grid.snap)
+local function adjust_width(by)
+    ext.grid.GRIDWIDTH = math.max(1, ext.grid.GRIDWIDTH + by)
+    hydra.alert("grid is now " .. tostring(ext.grid.GRIDWIDTH) .. " tiles wide", 1)
+    fnutils.map(window.visiblewindows(), snap)
 end
 
 local function adjust_focused_window(fn)
-  local win = window.focusedwindow()
-  local f = ext.grid.get(win)
-  fn(f)
-  ext.grid.set(win, f, win:screen())
+    local f = get_grid()
+    fn(f)
+    set_grid_and_screen(f)
 end
 
 function ext.grid.maximize_window()
-  local win = window.focusedwindow()
-  local f = {x = 0, y = 0, w = ext.grid.GRIDWIDTH, h = 2}
-  ext.grid.set(win, f, win:screen())
+    local f = {x = 0, y = 0, w = ext.grid.GRIDWIDTH, h = 2}
+    set_grid_and_screen(f)
 end
 
-function ext.grid.pushwindow_nextscreen()
-  local win = window.focusedwindow()
-  ext.grid.set(win, ext.grid.get(win), win:screen():next())
+local function screen_right()
+    local win = win or window.focusedwindow()
+    set_grid_and_screen(get_grid(), win:screen():next(), win)
+    adjust_focused_window(function(f)
+        f.x = 0
+    end)
 end
 
-function ext.grid.pushwindow_prevscreen()
-  local win = window.focusedwindow()
-  ext.grid.set(win, ext.grid.get(win), win:screen():previous())
+local function screen_left()
+    local win = win or window.focusedwindow()
+    set_grid_and_screen(get_grid(), win:screen():previous(), win)
+    adjust_focused_window(function(f)
+        f.x = ext.grid.GRIDWIDTH - f.w
+    end)
 end
 
-function ext.grid.pushwindow_left()
-  adjust_focused_window(function(f) f.x = math.max(f.x - 1, 0) end)
+local function go_down()
+    adjust_focused_window(function(f)
+        f.y = math.min(f.y + 1, ext.grid.GRIDHEIGHT)
+    end)
 end
 
-function ext.grid.pushwindow_right()
-  adjust_focused_window(function(f) f.x = math.min(f.x + 1, ext.grid.GRIDWIDTH - f.w) end)
-end
-
-function ext.grid.pushwindow_down()
-  adjust_focused_window(function(f)
-    f.y = math.min(f.y + 1, ext.grid.GRIDHEIGHT - f.y)
-  end)
-end
-
-function ext.grid.pushwindow_up()
-  adjust_focused_window(function(f)
-    f.y = math.min(f.y - 1, ext.grid.GRIDHEIGHT - f.y)
-  end)
+local function go_up()
+    adjust_focused_window(function(f)
+        f.y = math.min(f.y - 1, ext.grid.GRIDHEIGHT - f.y)
+    end)
 end
 
 -- Resize the window, or do nothing if we've hit a grid boundary.
@@ -120,49 +122,60 @@ local function shorter()
   end)
 end
 
--- ...resize bindings work differently when we're at the bottom or right screen
--- edges. Let's detect those cases.
-local function at_bottom()
-  local win = window.focusedwindow()
-  local f = ext.grid.get(win)
-  return f.y ~= 0 and f.y + f.h == ext.grid.GRIDHEIGHT
+-- Bindings work differently when we're at the screen edges.
+local function edge_is (testFunc)
+    local f = get_grid(window.focusedwindow())
+    return testFunc(f)
 end
-local function at_right()
-  local win = window.focusedwindow()
-  local f = ext.grid.get(win)
-  return f.x ~= 0 and f.x + f.w == ext.grid.GRIDWIDTH
-end
+local function at_left(f)
+    return f.x == 0 end
+local function at_bottom (f)
+    return f.y == ext.grid.GRIDHEIGHT - f.x end
+local function at_right(f)
+    return f.x == ext.grid.GRIDWIDTH - f.w end
 
 -- Here are the functions we'll actually bind.
-function ext.grid.resizewindow_right()
+local function go_left()
+    if edge_is(at_left) then screen_left()
+    else adjust_focused_window(function (f)
+        f.x = math.max(f.x - 1, 0)
+    end) end
+end
+
+local function go_right()
+    if edge_is(at_right) then screen_right()
+    else adjust_focused_window(function (f)
+        f.x = math.min(f.x + 1, ext.grid.GRIDWIDTH - f.w)
+    end) end
+end
+
+function ext.grid.resize_right()
   hydra.alert("→", 1)
-  if at_right(f) then thinner(f); ext.grid.pushwindow_right()
+  if at_right(f) then thinner(f); ext.grid.screen_right()
   else                wider(f) end
 end
-function ext.grid.resizewindow_left()
+function ext.grid.resize_left()
   hydra.alert("←", 1)
-  if at_right(f) then wider(f); ext.grid.pushwindow_left()
+  if at_right(f) then wider(f); ext.grid.screen_left()
   else                thinner(f) end
 end
-function ext.grid.resizewindow_up()
+function ext.grid.resize_up()
   hydra.alert("↑", 1)
-  if at_bottom(f) then taller(f); ext.grid.pushwindow_up()
+  if at_bottom(f) then taller(f); ext.grid.go_up()
   else                 shorter(f) end
 end
-function ext.grid.resizewindow_down()
+function ext.grid.resize_down()
   hydra.alert("↓", 1)
-  if at_bottom(f) then shorter(f); ext.grid.pushwindow_down()
+  if at_bottom(f) then shorter(f); ext.grid.go_down()
   else                 taller(f) end
 end
 
 -- And now we will actually bind them.
-ctrl_r:bind({}, "[", ext.grid.pushwindow_prevscreen)
-ctrl_r:bind({}, "]", ext.grid.pushwindow_nextscreen)
-ctrl_r:bind({}, "H", ext.grid.pushwindow_left)
-ctrl_r:bind({}, "J", ext.grid.pushwindow_down)
-ctrl_r:bind({}, "K", ext.grid.pushwindow_up)
-ctrl_r:bind({}, "L", ext.grid.pushwindow_right)
-ctrl_r:bind({"shift"}, "H", ext.grid.resizewindow_left)
-ctrl_r:bind({"shift"}, "J", ext.grid.resizewindow_down)
-ctrl_r:bind({"shift"}, "K", ext.grid.resizewindow_up)
-ctrl_r:bind({"shift"}, "L", ext.grid.resizewindow_right)
+ctrl_r:bind({}, "H", go_left)
+ctrl_r:bind({}, "L", go_right)
+ctrl_r:bind({}, "J", go_down)
+ctrl_r:bind({}, "K", go_up)
+ctrl_r:bind({"shift"}, "H", ext.grid.resize_left)
+ctrl_r:bind({"shift"}, "J", ext.grid.resize_down)
+ctrl_r:bind({"shift"}, "K", ext.grid.resize_up)
+ctrl_r:bind({"shift"}, "L", ext.grid.resize_right)
